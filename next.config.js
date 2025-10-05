a7d99ec0fb18b244768a147ae5e9e3667ba8f6d4
IamAKhtar <alikhanakhtar466@gmail.com>
2025-10-05 09:46:19 +0530

    Modified 2 files

--- next.config.js
+++ next.config.js
@@ -1,31 +1,19 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+// @ts-check
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/sw.js',
-        destination: '/_next/static/sw.js',
-      },
-    ];
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Service Worker
-    if (!isServer) {
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env.SW_VERSION': JSON.stringify(buildId),
-        })
-      );
-    }
-    return config;
-  },
+  experimental: { appDir: true },
+  reactStrictMode: true,
+  // No custom rewrites for /sw.js are needed; the plugin outputs sw.js into /public at build. 
 };
 
-module.exports = process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
